4ae67dfe1f7f0fc9f9d02b6b13647bb0359c9806
Vincent Nguyen <vincentnguyen1732@gmail.com>
2014-11-13 14:53:03 -0800

    Removing homework 3 files

--- Assignment1/src/util/BagException.java
+++ Assignment1/src/util/BagException.java
@@ -2,4 +2,11 @@ package util;
 
 public class BagException extends RuntimeException {
 
+	public BagException() {
+		super();
+	}
+	
+	public BagException(String message) {
+		super(message);
+	}
 }
